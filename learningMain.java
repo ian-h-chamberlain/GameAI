e46da8179c23dba178c0b85a9d3d08d32216eb14
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2017-02-18 16:04:28 -0500

    Basic evolution code outlined, should be ready to integrate with NN

--- learningMain.java
+++ learningMain.java
@@ -1,8 +1,10 @@
 package ch.idsia.agents.controllers;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 import java.util.TreeMap;
 
@@ -32,53 +34,91 @@ public final class learningMain
 		int numParents = 50;
 		int numChildren = 50;
 		
+		Random rn = new Random();
+		
 		TreeMap<Float, ArrayList<Integer>> networkFitnesses = new TreeMap<>();
-		ArrayList<Integer> parents = new ArrayList<>();
-		// TODO randomly initialize parents
+		ArrayList<Integer> curGeneration = new ArrayList<>();
+		
+		HashMap<Integer, Float> fitnesses = new HashMap<>();
+
+		int id = 0;
+		// randomly initialize parents
+		for (int i=0; i<numParents; i++) {
+			curGeneration.add(id);
+			fitnesses.put(id, rn.nextFloat() * 100);
+			id++;
+		}
 
 		final BasicTask basicTask = new BasicTask(cmdLineOptions);
 
+		// start running the generations
 		for (int i=0; i<numGenerations; i++) {
 			
-			// TODO Generate children randomly from parent list
+			System.out.println("Generation " + i);
 			
-			for (int j=0; j<numParents + numChildren; j++) {
+			// Generate children randomly from parent list
+			for (int j=0; j<numChildren; j++) {
+				int first = rn.nextInt(numParents);
+				int second = first;
+				while (second == first) {
+					second = rn.nextInt(numParents);
+				}
+				
+				// recombine second and first
+				curGeneration.add(id);
+				fitnesses.put(id, rn.nextFloat() * 100);
+				id++;
+				
+				// TODO apply random mutation to child
+			}
+			
+			networkFitnesses.clear();
+
+			for (int j=0; j<curGeneration.size(); j++) {
+				
 				// TODO set the current neural network
+				/*
 				basicTask.reset(cmdLineOptions);
 				basicTask.runOneEpisode();
 				
 				final MarioCustomSystemOfValues sov = new MarioCustomSystemOfValues();
 				float fitness = basicTask.getEnvironment().getEvaluationInfo().computeWeightedFitness(sov);
+				*/
+				float fitness = fitnesses.get(curGeneration.get(j));
 				
 				if (!networkFitnesses.containsKey(fitness)) {
 					networkFitnesses.put(fitness, new ArrayList<>());
 				}
 
-				networkFitnesses.get(fitness).add(j); // TODO actually store network
+				networkFitnesses.get(fitness).add(curGeneration.get(j)); // TODO actually store network
 			}
+			
+			System.out.println("Next parents, from " + networkFitnesses.size() + " pool");
 			
 			Iterator<Map.Entry<Float, ArrayList<Integer>>> itr = networkFitnesses.descendingMap().entrySet().iterator();
 			
-			// Get the next generation parents
-			parents.clear();
-			for (int j=0; j<numParents; j++) {
+			// Get the next generation parents with highest fitnesses
+			curGeneration.clear();
+			while (curGeneration.size() < numParents) {
 				if (!itr.hasNext()) {
 					// this probably shouldn't happen
 					System.err.println("Ran out of candidates!");
 					break;
 				}
 
-				ArrayList<Integer> nextParents = itr.next().getValue();
+				Map.Entry<Float, ArrayList<Integer>> entry = itr.next();
 				int k = 0;
-				while (k < nextParents.size() && j < numParents) {
-					parents.add(nextParents.get(k));
-					j++;
+				while (k < entry.getValue().size() && curGeneration.size() < numParents) {
+					// System.out.print(entry.getValue().get(k) +":" + entry.getKey().toString() + ",");
+					curGeneration.add(entry.getValue().get(k));
+					System.out.print(entry.getValue().get(k) + ":" + entry.getKey() + ",");
 					k++;
 				}
 			}
+			System.out.println("");
+			System.out.println(curGeneration.size() + " selected");
 			
 			// TODO: calculate std. devation and use it for evolving
-
 		}
 
 		System.exit(0);
