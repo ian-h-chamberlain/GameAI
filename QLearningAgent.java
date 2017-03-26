dafe445e82d789147b510110cbf7c54254a9f9f8
Connor Fahy <heskey333@gmail.com>
2017-03-26 15:25:45 -0400

    Starting work on QLearningAgent

--- QLearningAgent.java
+++ QLearningAgent.java
@@ -1,12 +1,25 @@
 package ch.idsia.agents.controllers;
 
+import java.util.Random;
+
 import ch.idsia.agents.Agent;
 
 public class QLearningAgent extends BasicMarioAIAgent implements Agent {
 
+	public float epsilon;
+	
+	QTable table;
+	
+	Random rand = new Random();
+	
 	public QLearningAgent() {
 		super("QLearningAgent");
 		reset();
+	}
+	
+	@Override
+	public boolean[] getAction() {
+		
 	}
 	
 	public void reset() {
