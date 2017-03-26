a153bf9581413cb522d2a9709f13fc60ee589357
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2017-03-26 14:34:23 -0400

    Added QLearningMain and gave the agent access to a q-table

--- QLearningAgent.java
+++ QLearningAgent.java
@@ -3,10 +3,16 @@ package ch.idsia.agents.controllers;
 import ch.idsia.agents.Agent;
 
 public class QLearningAgent extends BasicMarioAIAgent implements Agent {
+	
+	static QTable table;
 
 	public QLearningAgent() {
 		super("QLearningAgent");
 		reset();
+	}
+	
+	public static void setQTable(QTable t) {
+		table = t;
 	}
 	
 	public void reset() {
