f295ac0964e2da129401c1792c1a8d47dc690191
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2017-03-26 15:56:08 -0400

    Started outlining getState function

--- QLearningAgent.java
+++ QLearningAgent.java
@@ -5,6 +5,9 @@ import ch.idsia.agents.Agent;
 public class QLearningAgent extends BasicMarioAIAgent implements Agent {
 	
 	static QTable table;
+	int stateSize = 19;
+	boolean isStuck;
+	int framesSinceMoved = 0;
 
 	public QLearningAgent() {
 		super("QLearningAgent");
@@ -17,5 +20,25 @@ public class QLearningAgent extends BasicMarioAIAgent implements Agent {
 	
 	public void reset() {
 		
+	}
+	
+	boolean[] getState() {
+		boolean[] state = new boolean[stateSize];
+		
+		// state[0] is stuck?
+		
+		// state[1] is on the ground
+		
+		// state[2-4] detect small, medium, and large gaps in the level
+		
+		// state[5-8] are for obstacles directly in front of mario
+		
+		// state[9-18] are for a 3x3 grid of enemy detection around mario
+		
+		// POSSIBLE IDEAS
+		// state[19-21] for platforms above Mario
+		
+		
+		return state;
 	}
 }
