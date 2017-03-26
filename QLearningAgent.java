531244a84fde9a82f32f1415d0edb48a78d91b3b
Connor Fahy <heskey333@gmail.com>
2017-03-26 15:56:37 -0400

    More work on the agent.

--- QLearningAgent.java
+++ QLearningAgent.java
@@ -10,19 +10,42 @@ public class QLearningAgent extends BasicMarioAIAgent implements Agent {
 
 	public float epsilon;
 	
-	QTable table;
-	
 	Random rand = new Random();
+	
+	public boolean[] lastAction;
+	public boolean[] lastState;
 	
 	public QLearningAgent() {
 		super("QLearningAgent");
 		reset();
 	}
+	boolean[] getState(){
+		return null;
+	}
 	
 	@Override
 	public boolean[] getAction() {
-		
+		boolean[] ret;
+		boolean[] state = getState();
+		if(rand.nextFloat()> epsilon){
+			
+		}else{
+			ret = getRandomAction();
+		}
+		lastState = state;
+		lastAction = ret;
+		return ret;
 	}
+	
+	
+	boolean[] getRandomAction(){
+		boolean[] ret = new boolean[5];
+		for(int i = 0; i< ret.length; i++){
+			ret[i] = rand.nextBoolean();
+		}
+		return ret;
+	}
+	
 	public static void setQTable(QTable t) {
 		table = t;
 	}
